6a116e6d33e23288958cc4aacd632d53ec24246b
BRAHMENDER SHAIK <87408536+BrahmenderShaik@users.noreply.github.com>
2023-11-08 15:27:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 function sendData() {
-    var temperature = document.getElementById("temperature").value;
-    var humidity = document.getElementById("humidity").value;
+  // Get values from text boxes
+  var temperature = document.getElementById("temperature").value;
+  var humidity = document.getElementById("humidity").value;
+
+  // Construct the URL with the entered values
+  var url = "https://api.thingspeak.com/update?api_key=" + Apikey + "&field1=" + temperature + "&field2=" + humidity;
+
+  // Use AJAX to send data to Arduino
+  var xhr = new XMLHttpRequest();
   
-    // Use AJAX to send data to Arduino
-    var xhr = new XMLHttpRequest();
-    var url = "https://api.thingspeak.com/update?api_key=" + Apikey + "&field1=" + temperature + "&field2=" + humidity;
-  
-    xhr.open("GET", url, true);
-    xhr.send();
-  
-    xhr.onload = function () {
-      if (xhr.status == 200) {
-        alert("Data sent successfully!");
-      } else {
-        alert("Error sending data. Please try again.");
-      }
-    };
-  }
-  
+  xhr.open("GET", url, true);
+  xhr.send();
+
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      alert("Data sent successfully!");
+    } else {
+      alert("Error sending data. Please try again.");
+    }
+  };
+}
